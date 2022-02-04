9795b2f734c4ef86341c8355b74f5a0fe704896c
AlexKnyshov <alexknyshov@gmail.com>
2022-02-04 17:01:06 -0500

    cont and missing data

--- simulation/post_INDELible.R
+++ simulation/post_INDELible.R
@@ -7,31 +7,49 @@ df <- read.csv(df_path)
 
 nloci <- 2000
 
-#mkdir alignments2
+cmd0 <- "mkdir alignments2"
+system(cmd0)
 
 for (f in 1:nloci){
 	remaining_taxa <- as.character(eval(parse(text=df$remaining_taxa[f])))
 	seqpath <- paste0(alignment_folder_path, "/output_", df$loci[f], "_TRUE.phy")
-	print(seqpath)
 	locus <- read.dna(seqpath, format="sequential", as.character = T)
 	locus <- locus[rownames(locus) %in% remaining_taxa,]
+	#contamination processing
+	contaminant_tips <- eval(parse(text=df$cont_pairs[f]))
+	lencon <- length (contaminant_tips)
+	if (lencon > 0) {
+		for (t in seq(2,lencon,2)) {
+			locus[which(rownames(locus) == contaminant_tips[t]),] <- locus[which(rownames(locus) == contaminant_tips[t-1]),]
+		}
+	}
+	# missing data
+	loclen <- length(locus[1,])
 	taxa_vector <- as.character(eval(parse(text=df$taxa_missing_segments[f])))
-	start_vector <- eval(parse(text=df$missing_segments_start[f]))
-	end_vector <- eval(parse(text=df$missing_segments_end[f]))
-	# print (length(taxa_vector))
-	# print (length(start_vector))
-	# print (start_vector)
+	missing_segments_prop_vector <- eval(parse(text=df$missing_segments_prop[f]))
+	missing_segments_bias_vector <- eval(parse(text=df$missing_segments_bias[f]))
 	for (t in 1:length(taxa_vector)) {
-		# print(t)
 		taxon_name <- taxa_vector[t]
 		taxon_index <- which(rownames(locus) == taxon_name)
-		# print(start_vector[t])
-		locus[taxon_index, 1:start_vector[t]] <- rep("-",start_vector[t])
-		print (end_vector[t])
-		print (df$loclen[f])
-		print (length(locus[taxon_index, ]))
-		locus[taxon_index, end_vector[t]:df$loclen[f]] <- rep("-",df$loclen[f]-end_vector[t]+1)
+		gapLen <- loclen*missing_segments_prop_vector[t]
+		gapLen5 <- round(gapLen*missing_segments_bias_vector[t])
+		gapLen3 <- round(gapLen-gapLen5)
+		if (gapLen5 > 0){
+			locus[taxon_index, 1:gapLen5] <- rep("-",gapLen5)
+		}
+		if (gapLen3 > 0){
+			locus[taxon_index, (loclen-gapLen3+1):loclen] <- rep("-",gapLen3)
+		}
 	}
+	#complete gap position removal
+	badpos <- numeric()
+	for (pos in 1:loclen) {
+		if (unique(locus[,pos]) == "-") {
+			badpos <- c(badpos, pos)
+		}
+	}
+	locus <- locus[,-badpos]
+	#
 	locus <- as.DNAbin(locus)
 	write.FASTA(locus, paste0("alignments2/", df$loci[f],".fas"))
 }

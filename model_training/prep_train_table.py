5fca52c11153b7020bb26ff21f80b9421774fac8
AlexKnyshov <alexknyshov@gmail.com>
2022-06-22 16:18:45 -0400

    reformatted

--- model_training/prep_train_table.py
+++ model_training/prep_train_table.py
@@ -1,16 +1,15 @@
+'''
+A script to prepare a table for model training / prediction
+Concatenates assessed properties tables from several datasets
+and removes selected columns
+'''
 import sys
 import csv
 
-pathfile = sys.argv[1]
-if len(sys.argv) > 2:
-	columns_to_exclude = sorted([int(x) for x in sys.argv[2:]], reverse=True)
-	if 0 in columns_to_exclude or 1 in columns_to_exclude:
-		print ("should not be removing columns 0 or 1")
-		sys.exit()
-else:
-	columns_to_exclude = []
-
 def output_line(line, handle, columns_to_exclude):
+	'''
+	A function to exclude selected columns and NA vals
+	'''
 	if len(columns_to_exclude) > 0:
 		for idx in columns_to_exclude:
 			if idx < len(line):
@@ -19,23 +18,51 @@ def output_line(line, handle, columns_to_exclude):
 		rejoinedline = "\t".join(line)
 		print(rejoinedline, file=handle)
 
-print ("read path file")
-dsdict = {}
-with open(pathfile) as pathfilehandle:
-	for line in pathfilehandle:
-		dsdict[line.strip().split("/")[-2]] = line.strip()
 
-print ("parsing tables...")
-with open("combined_ML_tab.tsv", "w") as outhandle:
-	headerB = True
-	for dataset in dsdict:
-		with open(dsdict[dataset]) as dshandle:
-			reader = csv.reader(dshandle, delimiter='\t', quotechar='"')
-			header = reader.__next__()
-			if headerB:
-				output_line(header, outhandle, columns_to_exclude)
-				headerB = False
-			for line in reader:
-				line[0] = dataset+'_'+line[0]
-				output_line(line, outhandle, columns_to_exclude)
-print("done")
+def parse_path_file(pathfile, columns_to_exclude=[]):
+	'''
+	A function to parse input files and write output
+	'''
+	print ("read path file")
+	with open(pathfile) as pathfilehandle:
+		with open("combined_ML_tab.tsv", "w") as outhandle:
+			headerB = True
+			for pathline in pathfilehandle:
+				strippathline = pathline.strip()
+				basename = strippathline.split("/")[-2]
+				print ("parsing table",basename)
+				with open(strippathline) as fhandle:
+					reader = csv.reader(fhandle, delimiter='\t', quotechar='"')
+					header = reader.__next__()
+					if headerB:
+						output_line(header, outhandle, columns_to_exclude)
+						headerB = False
+					for readerline in reader:
+						readerline[0] = basename+'_'+readerline[0]
+						output_line(readerline, outhandle, columns_to_exclude)
+	print("done")
+
+
+def main():
+	'''
+	The main function
+	'''
+	if len(sys.argv) > 1:
+		pathfile = sys.argv[1]
+		if len(sys.argv) > 2:
+			columns_to_exclude = sorted([int(x) for x in sys.argv[2:]], reverse=True)
+			if 0 in columns_to_exclude:
+				print ("should not be removing column 0")
+			else:
+				print ("excluding", columns_to_exclude)
+				parse_path_file(pathfile, columns_to_exclude)
+		else:
+			parse_path_file(pathfile)
+	else:
+		print("python3 prep_train_table.py [file with a list of paths] ([columns])")
+		print("python3 prep_train_table.py train_list.txt")
+		print("python3 prep_train_table.py train_list.txt 1 2 5")
+
+
+if __name__ == "__main__":
+	main()

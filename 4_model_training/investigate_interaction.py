27a8e7a25133d9b34c9af96a4df98db4a27b1cb0
Alexandra Walling <alexandra.walling@uri.edu>
2024-04-27 19:33:33 -0400

    updated model training to 4_model_training

---
+++ 4_model_training/investigate_interaction.py
@@ -0,0 +1,242 @@
+'''
+A script to investigate feature interactions via random forest regressor
+Code is adopted and modified after Seppe "Macuyiko" vanden Broucke
+https://blog.macuyiko.com/post/2019/discovering-interaction-effects-in-ensemble-models.html
+https://blog.macuyiko.com/post/2021/revisiting-discovery-of-interaction-effects.html
+'''
+import sys
+import pandas as ps
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import argparse
+import itertools
+from sklearn import tree
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.inspection import partial_dependence
+from sklearn.inspection import plot_partial_dependence
+from sklearn.inspection import PartialDependenceDisplay
+
+
+def center(arr):
+    '''
+    A function to center an array
+    '''
+    return arr - np.mean(arr)
+
+
+def cartesian_product(*arrays):
+    '''
+    A function to obtain a cartesian product of arrays
+    '''
+    la = len(arrays)
+    dtype = np.result_type(*arrays)
+    arr = np.empty([len(a) for a in arrays] + [la], dtype=dtype)
+    for i, a in enumerate(np.ix_(*arrays)):
+        arr[...,i] = a
+    return arr.reshape(-1, la)
+
+
+def compute_f_vals_sklearn(model, X, feats=None, grid_resolution=100):
+    '''
+    A function to compute F values (partial dependence values)
+    using synthetic grid values
+    '''
+
+    def _pd_to_df(pde, feature_names):
+        df = ps.DataFrame(cartesian_product(*pde[1]))
+        rename = {i: feature_names[i] for i in range(len(feature_names))}
+        df.rename(columns=rename, inplace=True)
+        df['preds'] = pde[0].flatten()
+        return df
+
+    def _get_feat_idxs(feats):
+        return [tuple(list(X.columns).index(f) for f in feats)]
+
+    f_vals = {}
+    if feats is None:
+        feats = list(X.columns)
+
+    # Calculate partial dependencies for full feature set
+    pd_full = partial_dependence(
+        model, X, _get_feat_idxs(feats), 
+        grid_resolution=grid_resolution
+    )
+
+    # Establish the grid
+    df_full = _pd_to_df(pd_full, feats)
+    grid = df_full.drop('preds', axis=1)
+
+    # Store
+    f_vals[tuple(feats)] = center(df_full.preds.values)
+
+    # Calculate partial dependencies for [1..SFL-1]
+    for n in range(1, len(feats)):
+        for subset in itertools.combinations(feats, n):
+            pd_part = partial_dependence(
+                model, X, _get_feat_idxs(subset),
+                grid_resolution=grid_resolution
+            )
+            df_part = _pd_to_df(pd_part, subset)
+            joined = ps.merge(grid, df_part, how='left')
+            f_vals[tuple(subset)] = center(joined.preds.values)
+
+    return f_vals
+
+
+#compute partial dep manually - faster but less accurate
+def compute_f_vals_manual(model, X, feats=None):
+    '''
+    A function to compute F values (partial dependence values)
+    using actual data values (less precise but much faster)
+    '''
+    def _partial_dependence(model, X, feats):
+        P = X.copy()
+        for f in P.columns:
+            if f in feats: continue
+            P.loc[:,f] = np.mean(P[f])
+        # Assumes a regressor here, use return model.predict_proba(P)[:,1] for binary classification
+        return model.predict(P)
+
+    f_vals = {}
+    if feats is None:
+        feats = list(X.columns)
+
+    # Calculate partial dependencies for full feature set
+    full_preds = _partial_dependence(model, X, feats)
+    f_vals[tuple(feats)] = center(full_preds)
+
+    # Calculate partial dependencies for [1..SFL-1]
+    for n in range(1, len(feats)):
+        for subset in itertools.combinations(feats, n):
+            pd_part = _partial_dependence(model, X, subset)
+            f_vals[tuple(subset)] = center(pd_part)
+
+    return f_vals
+
+
+def compute_h_val(f_vals, selectedfeatures):
+    '''
+    A function to compute second order H measure
+    checking interactions btw selected features
+    '''
+    numer_els = f_vals[tuple(selectedfeatures)].copy()
+    denom_els = f_vals[tuple(selectedfeatures)].copy()
+    sign = -1.0
+    for n in range(len(selectedfeatures)-1, 0, -1):
+        for subfeatures in itertools.combinations(selectedfeatures, n):
+            numer_els += sign * f_vals[tuple(subfeatures)]
+        sign *= -1.0
+    numer = np.sum(numer_els**2)
+    denom = np.sum(denom_els**2)
+    return np.sqrt(numer/denom)
+
+
+# better way but doesnt combine
+# def compute_h_val(f_vals, selectedfeatures):
+#     numer_els = f_vals[tuple(selectedfeatures)].copy()
+#     denom_els = f_vals[tuple(selectedfeatures)].copy()
+#     for n in range(len(selectedfeatures)-1, 0, -1):
+#         for subfeatures in itertools.combinations(selectedfeatures, n):
+#             sign = -1 if n == selectedfeatures - 1 else +1
+#             numer_els += sign * f_vals[tuple(subfeatures)]
+#     numer = np.sum(numer_els**2)
+#     denom = np.sum(denom_els**2)
+#     return np.sqrt(numer/denom)
+
+
+def compute_h_val_any(f_vals, allfeatures, selectedfeature):
+    '''
+    A function to compute first order H measure
+    checking if a feature interacts with any other subset of features
+    '''
+    otherfeatures = list(allfeatures)
+    otherfeatures.remove(selectedfeature)
+    denom_els = f_vals[tuple(allfeatures)].copy()
+    numer_els = denom_els.copy()
+    numer_els -= f_vals[(selectedfeature,)]
+    numer_els -= f_vals[tuple(otherfeatures)]
+    numer = np.sum(numer_els**2)
+    denom = np.sum(denom_els**2)
+    return np.sqrt(numer/denom)
+
+
+def main():
+    '''
+    The main function
+    '''
+    parser = argparse.ArgumentParser(description='Train Random Forest',
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
+
+    required.add_argument('-i', metavar='table', help='input table file',
+                            dest="locus_data", required=True)
+    required.add_argument('-m', metavar='modelfile', help='input trained model',
+                            dest="modelfilename", required=True)
+    optional.add_argument('-t', metavar='N', help='number of threads',
+                            dest="n_jobs", type=int, default=1)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+    else:
+        args = parser.parse_args()
+        locus_data = ps.read_csv(vars(args)["locus_data"], sep='\t')
+        with open(vars(args)["modelfilename"], 'rb') as f:
+            rfr_tt = pickle.load(f)
+        n_jobs = vars(args)["n_jobs"]
+
+        #reformat data
+        df = locus_data.iloc[:,1:]
+        feats = df.columns.values.tolist()
+
+        #compute PD values
+        f_vals = compute_f_vals_manual(rfr_tt, df, feats=feats)
+
+        #compute first order H-values
+        print ("compute first order H-values")
+        subsets = []
+        h_vals = []
+        for subset in feats:
+            h_val = compute_h_val_any(f_vals, feats, subset)
+            subsets.append(subset)
+            h_vals.append(h_val)
+        df1 = ps.DataFrame(list(zip(subsets, h_vals)),
+                            columns =['features', 'h_vals'])
+        df1.to_csv("first_order_H_vals.csv", index=False, encoding='utf-8')
+
+        #compute second order H-values
+        print ("compute second order H-values")
+        subsets = []
+        h_vals = []
+        for n in [2,3]:
+            combs = itertools.combinations(feats, n)
+            print ("level:", n)
+            for subset in combs:
+                h_val = compute_h_val(f_vals, subset)
+                subsets.append(' x '.join(subset))
+                h_vals.append(h_val)
+            if n == 2:
+                #for pairwise interactions plot top 4
+                print ("plot PDs")
+                plt.rcParams.update({'figure.figsize': [4,3]})
+                subsets1 = np.array(subsets)
+                h_vals1 = np.array(h_vals)
+                k1 = 4
+                for subset1 in subsets1[h_vals1.argsort()[::-1][:k1]]:
+                    print (subset1)
+                    split_feats = [tuple(subset1.split(' x '))]
+                    PartialDependenceDisplay.from_estimator(rfr_tt, df, split_feats, kind='average', n_jobs=n_jobs)
+                    plt.tight_layout(pad=0.5)
+                    plt.savefig(subset1+".svg")
+        subsets = np.array(subsets)
+        h_vals = np.array(h_vals)
+        df2 = ps.DataFrame({'features':subsets[h_vals.argsort()[::-1]], 'h_vals':h_vals[h_vals.argsort()[::-1]]})
+        df2.to_csv("second_order_H_vals.csv", index=False, encoding='utf-8')
+
+
+if __name__ == "__main__":
+    main()
---
+++ 4_model_training/prep_train_table.py
@@ -0,0 +1,68 @@
+'''
+A script to prepare a table for model training / prediction
+Concatenates assessed properties tables from several datasets
+and removes selected columns
+'''
+import sys
+import csv
+
+def output_line(line, handle, columns_to_exclude):
+	'''
+	A function to exclude selected columns and NA vals
+	'''
+	if len(columns_to_exclude) > 0:
+		for idx in columns_to_exclude:
+			if idx < len(line):
+				line.pop(idx)
+	if "NA" not in line:
+		rejoinedline = "\t".join(line)
+		print(rejoinedline, file=handle)
+
+
+def parse_path_file(pathfile, columns_to_exclude=[]):
+	'''
+	A function to parse input files and write output
+	'''
+	print ("read path file")
+	with open(pathfile) as pathfilehandle:
+		with open("combined_ML_tab.tsv", "w") as outhandle:
+			headerB = True
+			for pathline in pathfilehandle:
+				strippathline = pathline.strip()
+				basename = strippathline.split("/")[-2]
+				print ("parsing table",basename)
+				with open(strippathline) as fhandle:
+					reader = csv.reader(fhandle, delimiter='\t', quotechar='"')
+					header = reader.__next__()
+					if headerB:
+						output_line(header, outhandle, columns_to_exclude)
+						headerB = False
+					for readerline in reader:
+						readerline[0] = basename+'_'+readerline[0]
+						output_line(readerline, outhandle, columns_to_exclude)
+	print("done")
+
+
+def main():
+	'''
+	The main function
+	'''
+	if len(sys.argv) > 1:
+		pathfile = sys.argv[1]
+		if len(sys.argv) > 2:
+			columns_to_exclude = sorted([int(x) for x in sys.argv[2:]], reverse=True)
+			if 0 in columns_to_exclude:
+				print ("should not be removing column 0")
+			else:
+				print ("excluding", columns_to_exclude)
+				parse_path_file(pathfile, columns_to_exclude)
+		else:
+			parse_path_file(pathfile)
+	else:
+		print("python3 prep_train_table.py [file with a list of paths] ([columns])")
+		print("python3 prep_train_table.py train_list.txt")
+		print("python3 prep_train_table.py train_list.txt 1 2 5")
+
+
+if __name__ == "__main__":
+	main()
